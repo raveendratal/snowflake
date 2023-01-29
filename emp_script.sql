8e82061cfb7d42fb69abe0f569a4db871f942242
TR Raveendra <32982143+raveendratal@users.noreply.github.com>
2023-01-29 16:32:24 +0530

    Update emp_script.sql

--- emp_script.sql
+++ emp_script.sql
@@ -39,3 +39,22 @@ INSERT INTO DEPT VALUES (10,'ACCOUNTING','NEW YORK'),
 (20,'RESEARCH','DALLAS'),
 (30,'SALES','CHICAGO'),
 (40,'OPERATIONS','BOSTON');
+
+--- Creating Table employee1
+create table employee1(id int,name string,loc string);
+--- insert data into employee1
+insert into employee1 values (1,'Ravi','Bangalore');
+insert into employee1 values (2,'Raj','Chennai');
+insert into employee1 values(3,'Mahesh','Pune');
+insert into employee1 values(4,'Sridhar','Mumbai');
+insert into employee1 values(5,'Prasad','Hyderabad');
+--- Creating table  employee2
+create table employee2(id int,name string,loc string,sal int);
+----insert data into employee2
+insert into employee2 values (1,'Ravi','Bangalore',5000);
+insert into employee2 values (2,'Raj','Chennai',6000);
+insert into employee2 values(3,'Mahesh','Pune',8000);
+insert into employee2 values(4,'Sindhu','Bangalore',8000);
+insert into employee2 values(5,'Vikranth','Chennai',7900);
+insert into employee2 values(6,'Reshwanth','Bangalore',9000);
+
